7096f30b080a2f4375cd67cf8c4b4f0770792690
Mirco Hilbert <mhilbert@users.noreply.github.com>
2026-09-04 13:04:17 +0200

    Compatibility with ILIAS 9
    Compatibility with ILIAS 9

--- plugin.php
+++ plugin.php
@@ -8,11 +8,11 @@
 
 // Unique plugin id and plugin version
 $id      = 'xcampc';
-$version = '1.1.0';
+$version = '1.2.0';
 
 // Supported ILIAS versions
 $ilias_min_version = '8.0';
-$ilias_max_version = '8.999';
+$ilias_max_version = '9.999';
 
 // Author & Contact information
 $responsible      = 'Martin Gorgas';
